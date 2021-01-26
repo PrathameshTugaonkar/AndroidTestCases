6477e01afd1fefc8679264615c597d16fca716af
Prathamesh <>
2021-01-26 12:09:47 +0530

    unitTesting

---
+++ app/src/test/java/com/example/testapp/UtilsTest.java
@@ -0,0 +1,44 @@
+package com.example.testapp;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Date;
+
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
+
+public class UtilsTest {
+
+    @Test
+    public void testIsEmailValid() {
+        String testEmail = "tprathamesh21@gmail.com";
+        Assert.assertThat(String.format("Email Validity Test failed for %s ", testEmail), Utils.checkEmailForValidity(testEmail), is(true));
+
+    }
+
+    @Test
+    public void testCheckDateWasConvertedCorrectly() {
+        long inMillis = System.currentTimeMillis();
+        Date date = Utils.calendarDate(inMillis);
+        assertEquals("Date time in millis is wrong",
+                inMillis * 1000, date.getTime());
+    }
+
+    @Test
+    public void testEmailValidityPartTwo() {
+        String testEmail = "   tprathamesh21@gmail.com  ";
+        Assert.assertThat(String.format("Email Validity Test failed for %s ", testEmail), Utils.checkEmailForValidity(testEmail), is(true));
+    }
+
+    @Test
+    public void emailStringNullCheck() {
+        Assert.assertThat(Utils.emailStringChecker(null), is(""));
+    }
+
+    @Test
+    public void emailStringEmptyCheck() {
+        Assert.assertThat(Utils.emailStringChecker(""), is(""));
+    }
+
+}
